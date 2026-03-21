5b26f58d9d71fd8324bbcfb3c1fcc67f78d7c4de
Konstantin-Volodin <63243086+Konstantin-Volodin@users.noreply.github.com>
2026-03-21 14:18:39 -0400

    update: initial authelia setup

--- start-services.sh
+++ start-services.sh
@@ -27,8 +27,24 @@ else
     echo "IMMICH_DB_PASSWORD=$(openssl rand -hex 128 | tr -d '\n')" >> .env
     echo "AUTHENTIK_DB_PASSWORD=$(openssl rand -hex 128 | tr -d '\n')" >> .env
     echo "AUTHENTIK_SECRET_KEY=$(openssl rand -hex 128 | tr -d '\n')" >> .env
+    echo "AUTHELIA_SESSION_SECRET=$(openssl rand -hex 64 | tr -d '\n')" >> .env
+    echo "AUTHELIA_STORAGE_ENCRYPTION_KEY=$(openssl rand -hex 64 | tr -d '\n')" >> .env
+    echo "AUTHELIA_OIDC_HMAC_SECRET=$(openssl rand -hex 64 | tr -d '\n')" >> .env
+    echo "IMMICH_OIDC_SECRET=$(openssl rand -hex 32 | tr -d '\n')" >> .env
+    echo "MEALIE_OIDC_SECRET=$(openssl rand -hex 32 | tr -d '\n')" >> .env
     ok ".env file created."
 fi
+
+# ===== generate authelia secret =====
+mkdir -p services/authelia/secrets
+docker run -d --rm -v "${PWD}/services/authelia/configuration.yml:/config/configuration.yml" -v "${PWD}/services/authelia/secrets:/config/secrets" --name authelia-secret authelia/authelia:latest generate-secret > /
+
+# # ===== generate authelia oidc private key =====
+# if [ ! -f services/authelia/config/oidc.pem ]; then
+#     info "generating Authelia OIDC private key..."
+#     openssl genrsa -out services/authelia/config/oidc.pem 2048
+#     ok "OIDC private key generated."
+# fi
 
 # ===== start services =====
 # info "starting pihole ..."
@@ -37,20 +53,31 @@ fi
 # docker compose --env-file ../../.env up -d
 # ok "pihole done."
 
+info "generating immich config..."
+set -a; source .env; set +a
+envsubst < services/immich/config/immich.json.tmpl > services/immich/config/immich.json
+ok "immich config generated."
+
 info "starting immich ..."
-cd services/immich
+cd ${PWD}/services/immich
 docker compose --env-file ../../.env down
 docker compose --env-file ../../.env up -d
 ok "immich up."
 
 info "starting mealie ..."
-cd ../../services/mealie
+cd ${PWD}/services/mealie
 docker compose --env-file ../../.env down
 docker compose --env-file ../../.env up -d
 ok "mealie up."
 
+info "starting authelia ..."
+cd ${PWD}/services/authelia
+docker compose --env-file ../../.env down
+docker compose --env-file ../../.env up -d
+ok "authelia up."
+
 # info "starting authentik ..."
-# cd ../../services/authentik
+# cd ${PWD}/services/authentik
 # docker compose --env-file ../../.env down
 # docker compose --env-file ../../.env up -d
 # ok "authentik up."
