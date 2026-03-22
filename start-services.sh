08d2e5cc8d82f1ba0990cce8a8ed395b997a5911
Konstantin-Volodin <63243086+Konstantin-Volodin@users.noreply.github.com>
2026-03-21 21:27:43 -0400

    update: authelia startup script

--- start-services.sh
+++ start-services.sh
@@ -22,22 +22,33 @@ if [ -f .env ]; then
     ok ".env file already exists."
 else
     cp .env.example .env
+
+    # taiscale OAuth secret - see tailscale admin console
     read -rp "  [?] Tailscale OAuth client secret: " ts_authkey
     echo "TS_AUTHKEY=${ts_authkey}?ephemeral=false" >> .env
+
+    # immich database password
     echo "IMMICH_DB_PASSWORD=$(openssl rand -hex 128 | tr -d '\n')" >> .env
-    echo "AUTHENTIK_DB_PASSWORD=$(openssl rand -hex 128 | tr -d '\n')" >> .env
-    echo "AUTHENTIK_SECRET_KEY=$(openssl rand -hex 128 | tr -d '\n')" >> .env
-    echo "AUTHELIA_SESSION_SECRET=$(openssl rand -hex 64 | tr -d '\n')" >> .env
-    echo "AUTHELIA_STORAGE_ENCRYPTION_KEY=$(openssl rand -hex 64 | tr -d '\n')" >> .env
-    echo "AUTHELIA_OIDC_HMAC_SECRET=$(openssl rand -hex 64 | tr -d '\n')" >> .env
-    echo "IMMICH_OIDC_SECRET=$(openssl rand -hex 32 | tr -d '\n')" >> .env
-    echo "MEALIE_OIDC_SECRET=$(openssl rand -hex 32 | tr -d '\n')" >> .env
+
+    # authelia secrets
+    echo "AUTHELIA_JWT_PASSWORD=$(openssl rand -hex 128 | tr -d '\n')" >> .env
+    echo "AUTHELIA_SESSION_SECRET=$(openssl rand -hex 128 | tr -d '\n')" >> .env
+    echo "AUTHELIA_STORAGE_ENCRYPTION_KEY=$(openssl rand -hex 128 | tr -d '\n')" >> .env
+    echo "AUTHELIA_OIDC_HMAC_SECRET=$(openssl rand -hex 128 | tr -d '\n')" >> .env
+    # echo "AUTHELIA_ADMIN_PASSWORD=$(openssl rand -hex 128 | tr -d '\n')" >> .env
+
+    # done
     ok ".env file created."
 fi
 
-# ===== generate authelia secret =====
+# ===== generate authelia admin =====
 mkdir -p services/authelia/secrets
-docker run -d --rm -v "${PWD}/services/authelia/configuration.yml:/config/configuration.yml" -v "${PWD}/services/authelia/secrets:/config/secrets" --name authelia-secret authelia/authelia:latest generate-secret > /
+docker run -d --rm -v "${PWD}/services/authelia/configuration.yml:/config/configuration.yml" -v "${PWD}/services/authelia/secrets:/config/secrets" --name "temp-authelia" authelia/authelia:latest sleep infinity
+docker exec temp-authelia authelia crypto hash generate --config "/config/configuration.yml" --password "authelia" | grep -oP '(?<=Digest: ).*'
+docker exec temp-authelia authelia crypto pair rsa generate --directory /config/secrets
+mv services/authelia/secrets/private.pem services/authelia/secrets/oidc.jwks.key
+mv services/authelia/secrets/public.pem services/authelia/secrets/oidc.jwks.pub
+docker stop temp-authelia
 
 # # ===== generate authelia oidc private key =====
 # if [ ! -f services/authelia/config/oidc.pem ]; then
@@ -75,9 +86,3 @@ cd ${PWD}/services/authelia
 docker compose --env-file ../../.env down
 docker compose --env-file ../../.env up -d
 ok "authelia up."
-
-# info "starting authentik ..."
-# cd ${PWD}/services/authentik
-# docker compose --env-file ../../.env down
-# docker compose --env-file ../../.env up -d
-# ok "authentik up."
